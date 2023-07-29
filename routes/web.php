4534734d07f24b1d259ea5a528be0aea7ca38d5e
NadaElnagar <nada.elnagar1994@gmail.com>
2023-07-29 20:05:56 +0300

    handle route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-$router->get('/import', 'TransactionController@import' );
+$router->get('/', 'TransactionController@import' );
 
 $router->group(['prefix' => 'api/v1'], function () use ($router) {
     $router->get('/transactaions','TransactionController@filter');
